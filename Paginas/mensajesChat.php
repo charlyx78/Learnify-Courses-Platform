0d71759ac437052d2e1bdc709955b63fb30789f5
Samuel Castro <samy.castrob@hotmail.com>
2023-03-27 20:07:28 -0600

    Ajax en mensajes de Chat

--- Paginas/mensajesChat.php
+++ Paginas/mensajesChat.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Learnify - Mensajes</title>
@@ -25,7 +26,7 @@
                         <div class="mensaje-usuario col-12 col-md-5 pe-5 align-self-start">
                             <div class="d-flex gap-2">
                                 <img src="../Imagenes/avatar.svg" class="rounded-circle" height="30" width="30" alt="">
-                                <p class="bg-terciario p-2 mb-1 rounded">
+                                <p class="bg-terciario p-2 mb-1 rounded" id="mensaje1">
                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque deserunt maiores ut reprehenderit officiis, modi quam voluptates iste eveniet unde nemo at iure sed sunt doloremque, nulla aliquam. Facere, at?
                                 </p>
                             </div>
@@ -49,17 +50,45 @@
             <div class="card-footer">
                 <form action="" class="mensaje">
                     <div class="d-flex align-items-start gap-2">
-                        <textarea name="mensaje" class="form-control w-100 h-auto" placeholder="Escribe un mensaje..."></textarea>
-                        <button type="submit" class="btn boton-secundario"><i class="bi bi-send-fill"></i></button>
+                        <textarea id= "escribirTexto" name="mensaje" class="form-control w-100 h-auto" placeholder="Escribe un mensaje..."></textarea>
+                        <button type="button" class="btn boton-secundario" onclick="pruebaUwU();"><i class="bi bi-send-fill"></i></button>
                     </div>
                 </form>
             </div>
         </div>
+
+        <div id="mensaje2"></div>
     </main>
 
     
     <?php include ('../footer.php'); ?>
-    <script src="../script.js"></script>
+    <!-- <script src="../script.js"></script> -->
+    
+    <script>
+        function pruebaUwU()
+        {
+            var texto = $('#escribirTexto').val().trim();
+            var parametros =
+            {
+                "texto" : texto
+            };
+
+            $.ajax({
+                
+                data: parametros,
+                url: 'mensajesAjax.php',
+                type: 'POST',
+
+                success : function(response)
+                {
+                    $('#mensaje1').html(response);
+                }
+            })
+        }
+
+
+
+    </script>
     
 </body>
 </html>
