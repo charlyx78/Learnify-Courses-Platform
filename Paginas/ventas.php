cb2bed0505dc17b85fad2d10eef531cc36fb202b
Carlos Adrián Ruiz Hernández <carh_080902@hotmail.es>
2023-05-02 18:01:23 -0600

    Pagina de ventas

--- Paginas/ventas.php
+++ Paginas/ventas.php
@@ -13,11 +13,41 @@
 
     <main class="container contenido mt-4">
 
-    <h2 class="mb-4">Tus cursos</h2>
-    <div id="carouselVentas" class="carousel slide" data-bs-ride="carousel" cycle="false">
+    <div id="carouselVentas" class="carousel slide" data-bs-ride="carousel" pause>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <div class="table-responsive">
+                <h2 class="mb-4">Tus cursos</h2>
+
+                <div class="card border-0 mb-4 bg-light">
+                    <div class="card-body">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-6">                            
+                                    <label for="dateDesde" class="form-label">Desde</label>
+                                    <input type="date" name="dateDesde" class="form-control mb-3"></div>
+                                <div class="col-6">
+                                    <label for="dateHasta" class="form-label">Hasta</label>
+                                    <input type="date" name="dateHasta" class="form-control mb-3">
+                                </div>
+                                <div class="col-6">
+                                    <label for="selectCategoria" class="form-label">Categoría</label>
+                                    <select name="" id="" class="form-select mb-3">
+                                        <option value="">Todos los cursos</option>
+                                    </select>
+                                </div>
+                                <div class="col-6 d-flex align-items-end">
+                                    <div class="mb-3 form-check">
+                                    <input type="checkbox" class="form-check-input" id="">
+                                    <label class="form-check-label" for="exampleCheck1">Solo cursos activos</label>
+                                    </div>
+                                </div>
+                                <div class="col-12"><button type="submit" class="btn boton-terciario w-100">Buscar</button></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="table-responsive" style="height: 600px;">
                     <table class="table table-light table-bordered">
                         <thead>
                             <th>Curso</th>
@@ -35,8 +65,28 @@
                         </tr>
                     </table>
                 </div>
+                <div class="d-flex justify-content-end">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                            </a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                            <a class="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                            </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
             <div class="carousel-item">
+                <h2 class="mb-4">Alumnos inscritos</h2>
                 <div class="table-responsive" style="height: 600px;">
                     <table class="table table-light table-bordered">
                         <thead>
